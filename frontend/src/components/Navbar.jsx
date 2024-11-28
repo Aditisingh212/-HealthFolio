235a43a1965a38aa5c0cc25c504ce9aa7ddbcd55
Aditi Singh <122527035+Aditisingh212@users.noreply.github.com>
2024-11-29 04:52:32 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+
+
 import React, { useContext, useState } from 'react'
 import {assets} from '../assets/assets'
 import { NavLink, useNavigate } from 'react-router-dom'
@@ -16,25 +18,25 @@ const Navbar = () => {
     }
 
   return (
-    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
-      <img onClick={()=> navigate('/')} className='w-44 cursor-pointer' src={assets.logo}/>
-
-      <ul className='hidden md:flex items-start gap-5 font-medium'>
+    <div className='flex items-center justify-between text-sm  mb-5 border-b border-b-gray-400'>
+      <img onClick={()=> navigate('/')} className=' h-15 w-40 cursor-pointer' src={assets.logo}/>
+      
+      <ul className='hidden md:flex items-start gap-8 font-medium'>
         <NavLink to='/'>
-            <li className='py-1'>HOME</li>
-            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+            <li className=' font-semibold text-lg text-gray-700'>HOME</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-5/5 m-auto hidden'/>
         </NavLink>
         <NavLink to='/doctors'>
-            <li className='py-1'>ALL DOCTORS</li>
-            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+            <li className=' font-semibold text-lg text-gray-700'>ALL DOCTORS</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-5/5 m-auto hidden'/>
         </NavLink>
         <NavLink to='/about'>
-            <li className='py-1'>ABOUT</li>
-            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+            <li className=' font-semibold text-lg text-gray-700'>ABOUT</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-5/5 m-auto hidden'/>
         </NavLink>
         <NavLink to= '/contact'>
-            <li className='py-1'>CONTACT US</li>
-            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+            <li className=' font-semibold text-lg text-gray-700'>CONTACT US</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-5/5 m-auto hidden'/>
         </NavLink>
       </ul>
 
@@ -42,17 +44,17 @@ const Navbar = () => {
       {
         token && userData
         ? <div className='flex items-center gap-2 cursor-pointer group relative'>
-            <img className='w-8 rounded-full' src={userData.image} alt="ProfilePic "></img>
-            <img className='w-2.5' src={assets.dropdown_icon}/>
+            <img className='w-12 rounded-full' src={userData.image} alt="ProfilePic "></img>
+            <img className='w-3' src={assets.dropdown_icon}/>
             <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block '>
-                <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                <div className='min-w-48 bg-stone-50 rounded flex flex-col gap-4 p-4'>
                     <p onClick={()=>navigate('/my-profile')}className='hover:text-black cursor-pointer'> My Profile</p>
                     <p onClick={()=>navigate('/my-appointments')}className='hover:text-black cursor-pointer'> My Appointments</p>
                     <p onClick={logout} className='hover:text-black cursor-pointer'> LogOut</p>
                 </div>
             </div>
         </div>
-        : <button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-md font-light hidden md:block hover:text-gray-900 hover:bg-green-200 '>Create Account</button>
+        : <button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-md font-light hidden md:block '>Create Account</button>
       }
 
       <img onClick={()=>setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon}></img>
@@ -67,7 +69,7 @@ const Navbar = () => {
             <NavLink onClick={()=>setShowMenu(false)} to='/'> <p className='px-4 py-2 rounded inline-block' >Home</p></NavLink>
             <NavLink onClick={()=>setShowMenu(false)} to='/doctors'> <p className='px-4 py-2 rounded inline-block' >All Doctors</p></NavLink>
             <NavLink onClick={()=>setShowMenu(false)} to='/about'> <p className='px-4 py-2 rounded inline-block' >About</p></NavLink>
-            <NavLink onClick={()=>setShowMenu(false)} to='/contact'><p className='px-4 py-2 rounded inline-block' >Contact</p></NavLink>
+            <NavLink onClick={()=>setShowMenu(false)} to='/contact'><p className='px-4 py-2 rounded inline-block' >Contact </p></NavLink>
         </ul>
       </div>
 
