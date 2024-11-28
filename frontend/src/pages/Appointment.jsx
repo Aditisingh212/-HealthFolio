2dc6ff1c74005d2df4a8a75c4a034ebab40373b2
aaditya0101 <123441639+aaditya0101@users.noreply.github.com>
2024-11-28 23:16:54 +0530

    Update Appointment.jsx

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -137,10 +137,10 @@ const Appointments = () => {
 
         <div className='flex-1 border border-gray-400 rounded-md p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0'>
           {/*------ Doc Info: name, degree, exper ------ */}
-          <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>{docInfo.name}
+          <p className='flex items-center gap-2 text-2xl font-bold text-gray-900'>{docInfo.name}
            <img className='w-5' src={assets.verified_icon}></img>
            </p>
-          <div className='flex items-center gap-2 text-sm mt-1 text-green-700'>
+          <div className='flex items-center gap-2 text-sm mt-1 font-semibold text-green-700'>
             <p>{docInfo.degree} - {docInfo.speciality} </p>
             <button className='py-0.5 px-2 border text-xs rounded-full'>{docInfo.experience}</button>
           </div>
