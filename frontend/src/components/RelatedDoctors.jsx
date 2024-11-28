a3258f71a743bd6fddec68a63e5a04c9e23843b4
aaditya0101 <123441639+aaditya0101@users.noreply.github.com>
2024-11-28 23:13:31 +0530

    Update RelatedDoctors.jsx

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -21,8 +21,8 @@ const RelatedDoctors = ({speciality,docId}) => {
       <p className='sm:w-1/3 text-center text-sm'>Simply Browse through our extensive list of trusted doctors.</p>
       <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
         {relDoc.slice(0,5).map((item,index)=>(
-            <div onClick={()=>{navigate(`/appointment/${item._id}`);scrollTo(0,0)}} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}> 
-              <img className='bg-blue-50' src={item.image}/>
+            <div onClick={()=>{navigate(`/appointment/${item._id}`);scrollTo(0,0)}} className='border border-primary rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}> 
+              <img className='bg-green-50' src={item.image}/>
                 <div className='p-4'>
                 <div className={`flex items-center gap-2 text-sm text-center ${item.available ? "text-green-500" : "text-gray-500"}`}>
                     <div className={`w-2 h-2 rounded-full ${item.available ? "bg-green-500" : "bg-gray-500"}`}></div>
@@ -34,7 +34,7 @@ const RelatedDoctors = ({speciality,docId}) => {
             </div>
         ))}
       </div>
-      <button onClick={()=>{navigate('/doctors/'); scrollTo(0,0) }} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>More</button>
+      <button onClick={()=>{navigate('/doctors/'); scrollTo(0,0) }} className='bg-green-200 text-gray-600 px-12 py-3 rounded-md font-semibold mt-10'>more</button>
     </div>
   )
 }
