0e4d5e7a6cfe3ee59b79e1ae09c42113b3527ff8
Aditi Singh <122527035+Aditisingh212@users.noreply.github.com>
2024-11-29 04:51:25 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,22 +1,23 @@
+
 import React from "react";
 import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
     <div className="md:mx-10">
-      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-10 my-10 mt-40 mb-5 text-sm">
         {/* ---Left--- */}
         <div>
             <img className='mb-5 w-40' src={assets.logo}></img>
-            <p className="w-full md:w-2/3 text-gray-600 leading-6">
-            At Prescripto, we believe that managing your health should be effortless, efficient, and accessible. We are here to bridge the gap between patients, doctors, and administrators, ensuring a smooth and transparent experience for everyone involved.
+            <p className="w-full md:w-3/3 text-gray-700 leading-6">
+            At HEALTHFOLIO, we believe that managing your health should be simple, efficient, and accessible. Our mission is to bridge the gap between patients, healthcare providers, and administrators, ensuring a seamless and transparent experience for everyone involved.
             </p>
         </div>
 
         {/* ---Center--- */}
         <div>
             <p className="text-xl font-medium mb-5">COMPANY</p>
-            <ul className="flex flex-col gap-2 text-gray-600">
+            <ul className="flex flex-col gap-2 text-gray-700">
                 <a href="/">Home</a>
                 <a href="/about">About Us</a>
                 <a href="/contact">Contact Us</a>
@@ -27,9 +28,9 @@ const Footer = () => {
         {/* ---Right--- */}
         <div>
             <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
-            <ul className="flex flex-col gap-2 text-gray-600">
+            <ul className="flex flex-col gap-2 text-gray-700">
                 <li>+91-78888-80XXX</li>
-                <li>prescripto@gmail.com</li>
+                <li>healthpholio@gmail.com</li>
             </ul>
         </div>
       </div>
@@ -37,7 +38,7 @@ const Footer = () => {
       <div>
         {/* Copright Text */}
         <hr/>
-        <p className="py-5 text-sm text-center">Copyright 2024@ Prescripto - All Rights Reserved.</p>
+        <p className="py-4 text-sm text-center">Copyright 2024@ HealthFolio - All Rights Reserved.</p>
       </div>
     </div>
   );
